8ccfe2befe4b85af12b68df0f43762e0eaa5b7d1
bgthomas <benjamin.guy.thomas@gmail.com>
2013-05-19 13:00:36 +0200

    Add basic test setup

---
+++ test/unit/test_movie.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../../lib/movie'
+
+describe Movie do
+  it "test" do
+    assert_equal true, false
+  end
+end
