4fb0549610892193276affc547dc159b8f799f9d
bgthomas <benjamin.guy.thomas@gmail.com>
2013-05-20 22:56:42 +0200

    Playing with iterations methods

--- lib/movie_finder.rb
+++ lib/movie_finder.rb
@@ -5,9 +5,9 @@ class MovieFinder
   MOVIES_DIR = File.expand_path MOVIES_ROOT
 
   def self.next_candidate
-    find_all.each do |movie_path|
+    # Use with a while loop
+    all_movies.each do |movie_path|
       movie = Movie.new(movie_path)
-      # try with if movie.has_encoded_version; next --- and then return the movie object
       unless movie.has_been_encoded_to_mp4?
         return movie
       end
@@ -15,9 +15,28 @@ class MovieFinder
     nil
   end
 
+  def self.candidates_for_encoding
+    # Use with a block
+    all_movies.each do |movie_path|
+      movie = Movie.new(movie_path)
+      yield movie unless movie.has_been_encoded_to_mp4?
+    end
+    nil
+  end
+
+  def self.all_candidates
+    # Use with #each
+    all_movies.map do |movie_path|
+      Movie.new(movie_path)
+    end.select do |movie|
+      !movie.has_been_encoded_to_mp4?
+    end
+  end
+
   private
 
-  def self.find_all
+  def self.all_movies
+    #TODO: filter *sample.mkv files"
     Dir.glob(MOVIES_DIR + "/**/*.mkv")
   end
 end
